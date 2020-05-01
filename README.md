f1910f68ac230749c855c1e791fd35063dd3141f
foqc <fabiaq44@gmail.com>
2020-04-30 22:28:30 -0500

    Water jug Riddle in JS

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# WaterJugRiddleJS
+# Water Jug Riddle
+This is a problem where, you have a 3-gallon and a 5-gallon jug that you can fill from a fountain of water. The problem is to fill one of the jugs with exactly 4 gallons of water.
+
+It is resolved with Javascript.
+
+## Why Did I Build This?
+
+This app was built for the purpose of sharing my knowledge on it, this is explained in my personal Blog.
+ 
+You can check out this post [here](https://quijosakaf.com/public/blog/acertijo-jarras-de-agua-en-prolog).
---
+++ WaterJugRiddleJS.js
@@ -0,0 +1,62 @@
+const MAX_BIG = 5
+const MAX_SMALL = 3
+
+const fillJug = (jugs, key = 'big', max = MAX_BIG) => ({ ...jugs, [key]: max })
+
+const emptyJug = (jugs, key = 'big') => ({ ...jugs, [key]: 0 })
+
+const bigToSmall = jugs => {
+    const { big, small } = jugs
+    const quantityNeededToFillSmall = MAX_SMALL - small
+
+    return {
+        big: big > quantityNeededToFillSmall ? big - quantityNeededToFillSmall : 0,
+        small: big > quantityNeededToFillSmall ? small + quantityNeededToFillSmall : small + big
+    }
+}
+
+const smallToBig = jugs => {
+    const { big, small } = jugs
+    const quantityNeededToFillBig = MAX_BIG - big
+
+    return {
+        big: small > quantityNeededToFillBig ? small - quantityNeededToFillBig : 0,
+        small: small > quantityNeededToFillBig ? big + quantityNeededToFillBig : small + big
+    }
+}
+
+const isRpeated = (path, { small, big }) => !!path.find(x => x.small === small && x.big === big)
+
+function getShortestPath(start, target) {
+
+    const queue = []
+    const path = []
+
+    path.push(start)
+    queue.push(path)
+
+    while (queue.length) {
+        const lastPath = queue.shift()
+        const lastState = lastPath[lastPath.length - 1]
+
+        if (target === lastState.big)
+            return lastPath
+
+        const states = new Set([fillJug(lastState), fillJug(lastState, 'small', MAX_SMALL),
+        bigToSmall(lastState), smallToBig(lastState), emptyJug(lastState), emptyJug(lastState, 'small')])
+
+        for (let item of states) {
+            if (!isRpeated(lastPath, item)) {
+                const newPath = [...lastPath]
+                newPath.push(item)
+                queue.push(newPath)
+            }
+        }
+    }
+
+    return null
+}
+
+path = getShortestPath({ small: 0, big: 0 }, 4)
+
+console.log(path)
